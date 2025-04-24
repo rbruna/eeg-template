4442c2dfa29112d70bffc0c3652ccff04eefa19c
rbruna <ricbruna@ucm.es>
2025-04-24 16:35:04 +0200

    Removing innecessary functions.

--- script/my_morph/dilateO2.m
+++
@@ -1,6 +0,0 @@
-function image = dilateO2 ( image )
-
-image = dilateR  ( image );
-image = dilateR  ( image );
-image = dilateC  ( image );
-image = dilateC  ( image );
--- script/my_morph/erodeO2.m
+++
@@ -1,6 +0,0 @@
-function image = erodeO2 ( image )
-
-image = erodeR   ( image );
-image = erodeR   ( image );
-image = erodeC   ( image );
-image = erodeC   ( image );
