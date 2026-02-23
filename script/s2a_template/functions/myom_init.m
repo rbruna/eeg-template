fca8473a220cbc7b92f634efc3f812945aef0a6d
rbruna <ricbruna@ucm.es>
2026-02-23 17:11:34 +0100

    Added code to add OpenMEEG to the search path in Windows

--- script/s2a_template/functions/myom_init.m
+++ script/s2a_template/functions/myom_init.m
@@ -14,6 +14,149 @@ if myom_inpath
     return
 end
 
+
+% Logic for looking for OpenMEEG in Windows.
+if ispc
+    
+    
+    % Looks for a folder named OpenMEEG in C:/Program Files.
+    for basedir = { 'C:/Program Files' }
+        
+        % Looks for an OpenMEEG folder in those locations.
+        output = dir ( sprintf ( '%s/OpenMEEG*', basedir {1} ) );
+        if isempty ( output ), continue, end
+        
+        % Lists the candidates.
+        candidates = strcat ( basedir {1}, '/', { output.name } );
+        
+        % Goes through each OpenMEEG folder candidates.
+        for candidate = candidates
+            
+            % Generates the binary folder.
+            bindir = sprintf ( '%s/bin', candidate {1} );
+    
+            % Looks for the OpenMEEG binaries.
+            if myom_inpath ( bindir )
+    
+                % Tries to run the binaries, if found.
+                if myom_checkbin ( bindir )
+    
+                    % If success, adds the folder to the path.
+                    myom_addpath ( bindir )
+                    
+                    % Prints the OpenMEEG location in screen.
+                    fprintf ( 1, 'Using OpenMEEG in: %s.\n', bindir )
+    
+                    % Exists silently.
+                    return
+                end
+            end
+        end
+    end
+
+
+% Logic for looking for OpenMEEG in Linux and Mac.
+else
+
+    % Looks for the binaries in /bin, /usr/bin, /usr/local/bin, and /opt/bin.
+    for candidate = { '/bin', '/usr/bin', '/usr/local/bin', '/opt/bin' }
+        
+        % Generates the binary folder.
+        bindir = candidate {1};
+        
+        % Looks for the binaries.
+        if myom_inpath ( bindir )
+        
+            % Tries to run the binaries, if found.
+            if myom_checkbin ( bindir )
+                
+                % If success, adds the folder to the path.
+                myom_addpath ( bindir )
+                
+                % Prints the OpenMEEG location in screen.
+                fprintf ( 1, 'Using OpenMEEG in: %s.\n', bindir )
+                
+                % Exists silently.
+                return
+            end
+        end
+    end
+    
+    
+    % Looks for a folder named OpenMEEG in /usr, /usr/local, and /opt.
+    for basedir = { '/usr', '/usr/local', '/opt' }
+        
+        % Looks for an OpenMEEG folder in those locations.
+        [ status, output ] = system ( sprintf ( 'find %s/* -maxdepth 0 -iname "openmeeg*"', basedir {1} ) );
+        if status ~= 0, continue, end
+        if isempty ( output ), continue, end
+        
+        % Lists the candidates.
+        candidates = splitlines ( strtrim ( output ) )';
+        
+        % Goes through each OpenMEEG folder candidates.
+        for candidate = candidates
+            
+            % Generates the binary folder.
+            bindir = sprintf ( '%s/bin', candidate {1} );
+    
+            % Looks for the OpenMEEG binaries.
+            if myom_inpath ( bindir )
+    
+                % Tries to run the binaries, if found.
+                if myom_checkbin ( bindir )
+    
+                    % If success, adds the folder to the path.
+                    myom_addpath ( bindir )
+                    
+                    % Prints the OpenMEEG location in screen.
+                    fprintf ( 1, 'Using OpenMEEG in: %s.\n', bindir )
+    
+                    % Exists silently.
+                    return
+                end
+            end
+        end
+    end
+    
+    
+    % If deep search, does in intensive search for the binaries.
+    if nargin && deep
+        
+        % Looks for the binaries in /usr and /opt.
+        for basedir = { '/usr', '/opt' }
+            
+            % Looks for the OpenMEEG binaries.
+            [ ~, output ] = system ( sprintf ( 'find %s/* -name om_assemble 2>/dev/null', basedir {1} ) );
+            if isempty ( output ), continue, end
+    
+            % Lists the candidates.
+            candidates = splitlines ( strtrim ( output ) )';
+    
+            % Goes through each OpenMEEG folder candidates.
+            for candidate = candidates
+                
+                % Gets the folder.
+                bindir = fileparts ( candidate {1} );
+    
+                % Tries to run the binaries, if found.
+                if myom_checkbin ( bindir )
+    
+                    % If success, adds the folder to the path.
+                    myom_addpath ( bindir )
+                    
+                    % Prints the OpenMEEG location in screen.
+                    fprintf ( 1, 'Using OpenMEEG in: %s.\nPlease, add it to the system search path.\n', bindir )
+    
+                    % Exists silently.
+                    return
+                end
+            end
+        end
+    end
+end
+
+
 % If Windows, raises an specific error.
 if ispc
     error ( [
@@ -21,105 +164,6 @@ if ispc
         'Please, follow the instructions in:\n' ...
         '%s' ], 'https://www.fieldtriptoolbox.org/faq/source/openmeeg/#installation-procedure-for-windows' )
 end
-
-
-% Looks for the binaries in /bin, /usr/bin, /usr/local/bin, and /opt/bin.
-for candidate = { '/bin', '/usr/bin', '/usr/local/bin', '/opt/bin' }
-    
-    % Generates the binary folder.
-    bindir = candidate {1};
-    
-    % Looks for the binaries.
-    if myom_inpath ( bindir )
-    
-        % Tries to run the binaries, if found.
-        if myom_checkbin ( bindir )
-            
-            % If success, adds the folder to the path.
-            myom_addpath ( bindir )
-            
-            % Prints the OpenMEEG location in screen.
-            fprintf ( 1, 'Using OpenMEEG in: %s.\n', bindir )
-            
-            % Exists silently.
-            return
-        end
-    end
-end
-
-
-% Looks for a folder named OpenMEEG in /usr, /usr/local, and /opt.
-for basedir = { '/usr', '/usr/local', '/opt' }
-    
-    % Looks for an OpenMEEG folder in those locations.
-    [ status, output ] = system ( sprintf ( 'find %s/* -maxdepth 0 -iname "openmeeg*"', basedir {1} ) );
-    if status ~= 0, continue, end
-    if isempty ( output ), continue, end
-    
-    % Lists the candidates.
-    candidates = splitlines ( strtrim ( output ) )';
-    
-    % Goes through each OpenMEEG folder candidates.
-    for candidate = candidates
-        
-        % Generates the binary folder.
-        bindir = sprintf ( '%s/bin', candidate {1} );
-
-        % Looks for the OpenMEEG binaries.
-        if myom_inpath ( bindir )
-
-            % Tries to run the binaries, if found.
-            if myom_checkbin ( bindir )
-
-                % If success, adds the folder to the path.
-                myom_addpath ( bindir )
-                
-                % Prints the OpenMEEG location in screen.
-                fprintf ( 1, 'Using OpenMEEG in: %s.\n', bindir )
-
-                % Exists silently.
-                return
-            end
-        end
-    end
-end
-
-
-% If deep search, does in intensive search for the binaries.
-if nargin && deep
-    
-    % Looks for the binaries in /usr and /opt.
-    for basedir = { '/usr', '/opt' }
-        
-        % Looks for the OpenMEEG binaries.
-        [ ~, output ] = system ( sprintf ( 'find %s/* -name om_assemble 2>/dev/null', basedir {1} ) );
-        if isempty ( output ), continue, end
-
-        % Lists the candidates.
-        candidates = splitlines ( strtrim ( output ) )';
-
-        % Goes through each OpenMEEG folder candidates.
-        for candidate = candidates
-            
-            % Gets the folder.
-            bindir = fileparts ( candidate {1} );
-
-            % Tries to run the binaries, if found.
-            if myom_checkbin ( bindir )
-
-                % If success, adds the folder to the path.
-                myom_addpath ( bindir )
-                
-                % Prints the OpenMEEG location in screen.
-                fprintf ( 1, 'Using OpenMEEG in: %s.\nPlease, add it to the system search path.\n', bindir )
-
-                % Exists silently.
-                return
-            end
-        end
-    end
-end
-
 
 % If not found, raises an error.
 error ( [
@@ -135,7 +179,11 @@ function result = myom_checkbin ( ompath )
 if nargin == 0, ompath = ''; end
 
 % Tries to run OpenMEEG binaries.
-[ status, msg ] = system ( sprintf ( 'export PATH="%s:$PATH"; om_assemble', ompath ) );
+if ispc
+    [ status, msg ] = system ( sprintf ( 'PATH=%%PATH%%;%s; && om_assemble', ompath ) );
+else
+    [ status, msg ] = system ( sprintf ( 'export PATH="%s:$PATH"; om_assemble', ompath ) );
+end
 
 % If success or probing a new path, returns the result.
 if status == 0 || nargin > 0
@@ -158,7 +206,7 @@ if nargin == 0, ompath = ''; end
 
 % Looks for the OpenMEEG binaries.
 if ispc
-    [ status, ~ ] = system ( sprintf ( 'export PATH="%s:$PATH"; where om_assemble', ompath ) );
+    [ status, ~ ] = system ( sprintf ( 'PATH=%%PATH%%;%s; && where om_assemble', ompath ) );
 else
     [ status, ~ ] = system ( sprintf ( 'export PATH="%s:$PATH"; which om_assemble', ompath ) );
 end
@@ -186,7 +234,11 @@ end
 
 
 % Adds the binary folder.
-setenv ( 'PATH', sprintf ( '%s:%s', getenv ( 'PATH' ), bindir ) )
+if ispc
+    setenv ( 'PATH', sprintf ( '%s;%s;', getenv ( 'PATH' ), bindir ) )
+else
+    setenv ( 'PATH', sprintf ( '%s:%s', getenv ( 'PATH' ), bindir ) )
+end
 
 % Adds the library folder.
 if isunix
