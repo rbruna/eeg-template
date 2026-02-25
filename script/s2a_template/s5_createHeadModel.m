68f2e0c7b5f654c31cc0538df3c7cef6fca44f49
rbruna <ricbruna@ucm.es>
2026-02-25 19:35:30 +0100

    Reordered to match sources reconstruction pipeline in the meeg_analysis repository.

--- script/s2a_template/s5_createHeadModel.m
+++ script/s2a_template/s5_createHeadModel.m
@@ -3,17 +3,23 @@ clear
 close all
 
 % Defines the location of the files.
+config.path.mri  = '../../template/anatomy/';
 config.path.head = '../../template/headmodel/';
 config.path.patt = '*.mat';
 
-% If the data will only be used for EEG, trims the head matrix.
-config.trim      = true;
+% Defines the head model. Can be 'bem3', 'ss' (single shell) or 'auto'.
+config.model     = 'bem3';
 
-% If no dipole fitting, removes the head matrix after calculating hm\dsm.
-config.clean     = true;
+% Defines the template grid to use.
+config.sources   = '../../template/grid/MNI-surface.mat';
+config.sources   = '../../template/grid/CTB-10mm.mat';
 
 % Action when the task have already been processed.
 config.overwrite = false;
+
+
+% Creates the output folder, if required.
+if ~exist ( config.path.head, 'dir' ), mkdir ( config.path.head ); end
 
 
 % Adds the functions folders to the path.
@@ -26,90 +32,119 @@ myft_path
 % Adds the FT toolboxes that will be required.
 ft_hastoolbox ( 'spm8', 1, 1 );
 ft_hastoolbox ( 'openmeeg', 1, 1 );
+ft_hastoolbox ( 'freesurfer', 1, 1 );
 
-% Sets OpenMEEG in silent mode.
-myom_verbosity (0)
+
+% Loads the sources template MRI and grid.
+srctemp = load ( config.sources );
 
 
 % Gets the files list.
-files            = dir ( sprintf ( '%s%s', config.path.head, config.path.patt ) );
+files = dir ( sprintf ( '%s%s', config.path.mri, config.path.patt ) );
 
 % Goes through all the files.
 for findex = 1: numel ( files )
     
     % Pre-loads the anatomy.
-    headdata         = load ( sprintf ( '%s%s', config.path.head, files ( findex ).name ), 'subject', 'model', 'sources' );
+    mridata           = load ( sprintf ( '%s%s', config.path.mri, files ( findex ).name ), 'subject', 'mesh' );
     
-    fileinfo         = whos ( '-file', sprintf ( '%s%s', config.path.head, files ( findex ).name ) );
-    if ismember ( 'headmodel', { fileinfo.name } ) && ~config.overwrite
-        fprintf ( 1, 'Ignoring subject ''%s'', head model ''%s'', sources model ''%s'' (already calculated).\n', headdata.subject, headdata.model, headdata.sources );
+    if strcmp ( config.model, 'ss' )
+        meshtype          = 'singleshell';
+    else
+        meshtype          = mridata.mesh.type;
+    end
+    
+    if exist ( sprintf ( '%s%s_%s_%s.mat', config.path.head, mridata.subject, meshtype, srctemp.model ), 'file' ) && ~config.overwrite
+        fprintf ( 1, 'Ignoring subject %s (already calculated).\n', mridata.subject );
         continue
     end
     
-    % Loads the anatomy except for the source model, if present.
-    headdata         = load ( sprintf ( '%s%s', config.path.head, files ( findex ).name ), '-regexp', '^(?!headmodel)\w' );
+    
+    fprintf ( 1, 'Working on subject %s.\n', mridata.subject );
+    
+    % Loads the anatomy and the surface mesh(es).
+    mridata           = load ( sprintf ( '%s%s', config.path.mri, files ( findex ).name ), 'subject', 'mri', 'landmark', 'transform', 'mesh', 'scalp' );
+    mri               = mridata.mri;
+    transform         = mridata.transform;
+    mesh              = mridata.mesh;
+    
+    % Unpacks the MRI.
+    mri               = my_unpackmri ( mri );
     
     
-    fprintf ( 1, 'Working on subject ''%s'', head model ''%s'', sources model ''%s''.\n', headdata.subject, headdata.model, headdata.sources );
-    
-    % Initializes the head model to the geometrical definition of the head.
-    headmodel        = headdata.mesh;
-    
-    
-    fprintf ( 1, '  Creating the volumen conductor.\n' );
-    
-    % If single shell uses only the brain surface.
-    if ismember ( headmodel.type, { 'singleshell' } )
-        
-        % Gets only the brain surface.
-        headmodel.type   = 'singleshell';
-        headmodel.bnd    = headmodel.bnd    ( strcmp ( headmodel.tissue, 'brain' ) );
-        headmodel.tissue = headmodel.tissue ( strcmp ( headmodel.tissue, 'brain' ) );
-        
-%         % Creates the volume conduction model using FT-singleshell.
-%         cfg              = [];
-%         cfg.method       = 'singleshell';
-%         headmodel        = ft_prepare_headmodel ( cfg, headmodel );
-        
-        % Initializes the volume conductor.
-        headmodel        = myss_headmodel ( headmodel );
+    % Sanitizes the surface meshes.
+    if strcmp ( config.model, 'bem3' ) && ~strncmp ( mesh.type, 'bem', 3 )
+        fprintf ( 1, 'Ignoring subject %s (not valid for three-layer BEM).\n', mridata.subject );
+        continue
     end
     
-    % If BEM uses the three surfaces.
-    if ismember ( headmodel.type, { 'bem3', 'bem3FT', 'bem3NFT' } )
+    % Converts the surface into single-shell, if requested.
+    if strcmp ( config.model, 'ss' ) && ~strcmp ( mesh.type, 'ss' )
         
-        % Checks that all the surfaces are available.
-        if ~all ( ismember ( { 'brain', 'skull', 'scalp' }, headmodel.tissue ) )
-            error ( 'Not all the surfaces required by BEM are available. Aborting.' );
-        end
-        
-        % Initializes the volume conduction model using OpenMEEG.
-        headmodel.cond   = [ 1 1/80 1 ] / 3;
-        headmodel        = myom_headmodel ( headmodel );
-        
-        % The size of the head matrix is:
-        % # of points + # of triangles - # of triangles in the outer mesh.
-        
-        % Creates the sources matrix using OpenMEEG.
-        headmodel        = myom_sourcematrix ( headmodel, headdata.grid );
-%         headmodel.dsm    = myom_dsm ( headmodel, headdata.grid );
-%         headmodel.grid   = headdata.grid;
-        
-        % Calculates hm\dsm.
-        headmodel        = myom_build_src ( headmodel, config.clean );
-        
-        % Trims the head matrix for EEG, if requested.
-        if config.trim
-            headmodel        = myom_trimmat ( headmodel );
-        end
+        % Gets only the brain surface mesh.
+        hit               = strcmp ( mesh.tissue, 'brain' );
+        mesh.type         = 'singleshell';
+        mesh.tissue       = mesh.tissue ( hit );
+        mesh.bnd          = mesh.bnd ( hit );
     end
     
     
-    fprintf ( 1, '  Saving the volume conductor.\n' );
-
-    % Adds the head model to the data.
-    headdata.headmodel = headmodel;
+    fprintf ( 1, '  Transforming the template (MNI) sources model to subject space.\n' );
+    
+    % Gets the template (MNI) sources model.
+    srcmodel          = srctemp.sourcemodel;
+    
+    % Stores the original source definition.
+    srcmodel.posori  = srcmodel.pos;
+    if isfield ( srcmodel, 'nrm' )
+        srcmodel.nrmori   = srcmodel.nrm;
+    end
+    
+    % Transforms the sources model to subject's native space.
+    srcmodel          = ft_convert_units ( srcmodel, transform.unit );
+    srcmodel          = ft_transform_geometry ( transform.mni2nat, srcmodel );
+    srcmodel          = ft_convert_units ( srcmodel, 'm' );
+    
+    
+    % Moves the sources inside the brain surface for BEM methods.
+    if strncmp ( mesh.type, 'bem', 3 )
+        
+        fprintf ( 1, '  Moving the sources inside the brain surface.\n' );
+        
+        % Creates a grid containing only the sources inside the brain.
+        tmpgrid           = srcmodel;
+        tmpgrid.pos       = tmpgrid.pos ( tmpgrid.inside, : );
+        tmpgrid.inside    = true ( size ( tmpgrid.pos, 1 ), 1 );
+        
+        % Moves all the sources of the grid inside the brain surface.
+        cfg               = [];
+        cfg.sourcemodel   = tmpgrid;
+        cfg.headmodel     = mesh;
+        cfg.headmodel.type = 'openmeeg';
+        cfg.moveinward    = 0.001;
+        cfg.inwardshift   = 0;
+        
+        tmpgrid           = ft_prepare_sourcemodel ( cfg );
+        
+        % Replaces the position of the sources inside the brain.
+        srcmodel.pos ( srcmodel.inside, : ) = tmpgrid.pos;
+    end
+    
+    
+    fprintf ( 1, '  Saving the sources model.\n' );
+    
+    % Creates the head model.
+    headdata           = [];
+    headdata.subject   = mridata.subject;
+    headdata.model     = mesh.type;
+    headdata.sources   = srctemp.model;
+    headdata.mri       = mridata.mri;
+    headdata.landmark  = mridata.landmark;
+    headdata.transform = mridata.transform;
+    headdata.mesh      = mesh;
+    headdata.scalp     = mridata.scalp;
+    headdata.grid      = srcmodel;
     
     % Saves the head model.
-    save ( '-v6', sprintf ( '%s%s_%s_%s', config.path.head, headdata.subject, headdata.model, headdata.sources ), '-struct', 'headdata' );
+    save ( '-v6', sprintf ( '%s%s_%s_%s.mat', config.path.head, headdata.subject, headdata.model, headdata.sources ), '-struct', 'headdata' );
 end
